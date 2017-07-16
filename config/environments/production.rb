69327bd1cc8e0154a01744dcc8b719f37f8852fa
jizavala <jig.zavala@gmail.com>
2017-07-15 19:48:28 -0500

    remove paperlicp.options command_path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  Paperclip.options[:image_magick_path] = Figaro.env.heroku_imagemagick_path
 end
