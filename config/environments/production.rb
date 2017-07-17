66af98b347f4a6809c6a81e8bdb78421fb62a819
jizavala <jig.zavala@gmail.com>
2017-07-17 12:03:19 -0500

    Se agrego una linea para hacer un merge de paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Paperclip.options[:image_magick_path] = Figaro.env.heroku_imagemagick_path
+  Paperclip.options.merge!(:command_path => "/usr/bin")
 
   Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
   Paperclip::Attachment.default_options[:url] = ':s3_host_name'
